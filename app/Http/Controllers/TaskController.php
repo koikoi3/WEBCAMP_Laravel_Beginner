ba28047a3785a7885018999233ad141c45913e3d
koikoi3 <trikoi35@icloud.com>
2022-11-11 11:58:14 +0000

    タスクの登録処理まで書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 namespace APP\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TaskRegisterPostRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Task as TaskModel;
 
 class TaskController extends Controller
 {
@@ -15,4 +18,33 @@ class TaskController extends Controller
     {
         return view('task.list');
     }
+    /**
+     * タスクの新規登録
+     */
+    public function register(TaskRegisterPostRequest $request)
+    {
+        // validate済のデータ取得
+        $datum = $request->validated();
+        //
+        //$user = Auth::user();
+        //$id = Auth::id();
+        //var_dump($datum, $user, $id); exit;
+        
+        // user_id の追加
+        $datum['user_id'] = Auth::id();
+        
+        // テーブルへのINSERT
+        $r = TaskModel::create($datum);
+var_dump($r); exit;
+    } catch(\Throwable $e) {
+        // XXX 本当はログに書く等の処理をする。今回は一端「出力する」だけ
+        echo $e->getMessage();
+        exit;
+    }
+    
+    // タスクの登録成功
+    $request->session()->flash('front.task_register_success', true);
+    
+    //
+    return redirect('/task/list');
 }
